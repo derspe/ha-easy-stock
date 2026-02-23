d5b7165f43218c4db77e9f74250739c86c7f965e
Frank Specht <der.spe@gmail.com>
2026-02-23 21:07:33 +0100

    Add options flow for name and scan interval
    Allows changing friendly name and scan interval after initial setup
    via the integration's Configure button. Default scan interval raised
    from 300 s to 900 s to reduce Yahoo Finance API load.
    
    Also adds YAHOO_CHART_URL_MINI constant (5-day fetch) in preparation
    for the coordinator refactor.

--- custom_components/easy_stock/config_flow.py
+++ custom_components/easy_stock/config_flow.py
@@ -8,6 +8,11 @@ from .const import DOMAIN, CONF_SYMBOL, CONF_NAME, CONF_SCAN_INTERVAL, DEFAULT_S
 class EasyStockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return EasyStockOptionsFlow(config_entry)
+
     async def async_step_user(self, user_input: dict | None = None):
         errors = {}
 
@@ -36,3 +41,31 @@ class EasyStockConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+
+class EasyStockOptionsFlow(config_entries.OptionsFlow):
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self._config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict | None = None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        current_name = self._config_entry.options.get(
+            CONF_NAME, self._config_entry.data.get(CONF_NAME, "")
+        )
+        current_interval = self._config_entry.options.get(
+            CONF_SCAN_INTERVAL, self._config_entry.data.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+        )
+
+        schema = vol.Schema(
+            {
+                vol.Optional(CONF_NAME, default=current_name): str,
+                vol.Optional(CONF_SCAN_INTERVAL, default=current_interval): vol.All(
+                    int, vol.Range(min=60, max=86400)
+                ),
+            }
+        )
+
+        return self.async_show_form(step_id="init", data_schema=schema)
--- custom_components/easy_stock/const.py
+++ custom_components/easy_stock/const.py
@@ -4,9 +4,14 @@ CONF_SYMBOL = "symbol"
 CONF_NAME = "name"
 CONF_SCAN_INTERVAL = "scan_interval"
 
-DEFAULT_SCAN_INTERVAL = 300  # 5 minutes
+DEFAULT_SCAN_INTERVAL = 900  # 15 minutes
 
 YAHOO_CHART_URL = (
     "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
     "?range=1y&interval=1d&includePrePost=false"
+)
+
+YAHOO_CHART_URL_MINI = (
+    "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
+    "?range=5d&interval=1d&includePrePost=false"
 )
